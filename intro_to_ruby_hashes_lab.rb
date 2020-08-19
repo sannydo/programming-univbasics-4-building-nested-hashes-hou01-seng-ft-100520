3931152c40ea11286efe68fa2ae21593f4214e4d
Sanny Do <sanny.do94@gmail.com>
2020-08-19 22:14:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,11 +60,11 @@ def monopoly_with_fourth_tier
      
      }
      :b_and_o_railroad  
-     
+    { 
      :mortgage_value [$400]
      
      :shortline_railroad 
      
      :mortgage_value [$800]
-   }
+    }
 
