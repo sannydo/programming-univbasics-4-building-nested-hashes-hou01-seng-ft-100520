4811aeb8e0a03c7fc84b716bc1446a6d01b81bfc
Sanny Do <sanny.do94@gmail.com>
2020-08-19 22:05:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,8 @@ def monopoly_with_third_tier
    :pieces [4]
    :rent_in_dollars 
    {
-     one_piece_owned: => "25",
-     two_pieced_owned: =>"50",
+     one_piece_owned: "25",
+     two_pieced_owned: "50",
      three_pieces_owned: => "100",
      four_pieces_owned: => "200"
    end
