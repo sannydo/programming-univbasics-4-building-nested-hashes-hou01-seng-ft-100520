2c35aea26d94132d40ea22afa4709da6d06c091d
Sanny Do <sanny.do94@gmail.com>
2020-08-19 21:57:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,22 +17,20 @@ def monopoly_with_third_tier
    :pieces [4]
    :rent_in_dollars 
    {
-     :one_piece_owned => 25
-    } 
-     :two_pieced_owned => 50
-     :three_pieces_owned  =>100
-     :four_pieces_owned => 200
+     :one_piece_owned => "25",
+     :two_pieced_owned => "50",
+     :three_pieces_owned  => "100",
+     :four_pieces_owned => "200"
    }
    :names  
    {
-     :reading_railroad =>
+     :reading_railroad 
    
      :pennsylvania_railroad 
     
-    } :b_and_o_railroad 
-   {
+     :b_and_o_railroad 
+   
      :shortline_railroad 
-    =>
   }
  }
 }
@@ -42,31 +40,31 @@ def monopoly_with_fourth_tier
  :railroads  
    :pieces [4]
    :rent_in_dollars 
-   
-      :one_piece_owned => 25
-    } :two_pieced_owned => 50
-     :three_pieces_owned => 100
-    } :four_pieces_owned =>200
-   
+   {
+     :one_piece_owned => "25",
+     :two_pieced_owned => "50",
+     :three_pieces_owned => "100",
+     :four_pieces_owned => "200"
+    }
    :names 
    {
      :reading_railroad  
-    =>
+    
      :mortgage_value => $100
-     {
-     :pennsylvania_railroad => 
-      =>
-     :mortgage_value => $200
-     {
-     :b_and_o_railroad  =>
-     } =>
-       :mortgage_value => $400
-     {
-     :shortline_railroad =>
      
-       :mortgage_value } $800
-       {
+     :pennsylvania_railroad 
+      
+     :mortgage_value => $200
+     
+     :b_and_o_railroad  
+     
+     :mortgage_value => $400
+     
+     :shortline_railroad 
+     
+     :mortgage_value => $800
    }
+  }
  
 end
 
